853dafbb93e62404ad0b1e57e4750049c51dfb59
Caden Jamason <me@lutrine.dev>
2023-07-24 14:33:23 -0700

    🎨 make it look pretty !!

--- chapter 1/EnterANumber/main.cpp
+++ chapter 1/EnterANumber/main.cpp
@@ -4,9 +4,10 @@ int main()
 {
     std::cout << "Enter a number: "; // ask user for a number
 
-    int x{ }; // define variable x to hold user input (and zero-initialize it)
+    int x{}; // define variable x to hold user input (and zero-initialize it)
     std::cin >> x; // get number from keyboard and store it in variable x
 
     std::cout << "You entered " << x << '\n';
+
     return 0;
 }
