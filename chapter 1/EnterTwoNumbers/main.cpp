e1f5c36887b5ab5e6461512bddd3a81d81346ebe
Caden Jamason <me@lutrine.dev>
2023-07-24 14:20:53 -0700

    ✨ add code relating to iostream [chapter 1.5]

---
+++ chapter 1/EnterTwoNumbers/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream> // for std::cout and std::cin
+
+int main() {
+    std::cout << "Enter two numbers separated by a space: ";
+
+    int x{}; // define variable x to hold user input (and zero-initialize it)
+    int y{}; // define variable y to hold user input (and zero-initialize it)
+    std::cin >> x >> y; // get two numbers and store it in variable x and y respectively
+
+    std::cout << "You entered " << x << " and " << y << "\n";
+
+    return 0;
+}
